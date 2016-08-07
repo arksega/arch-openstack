3500684fa3d0270b483f73158baa0488da43e51a
Sergio Gutierrez <ark.sega@gmail.com>
2016-08-07 12:31:45 -0500

    Fixing config file locations for httpd

--- identity_service.sh
+++ identity_service.sh
@@ -3,6 +3,7 @@
 
 # Root path
 root_path=$(pwd)
+
 # Create kesytone db and user
 mysql -u root -p$MARIADB_PASS << EOF
 CREATE DATABASE keystone;
@@ -15,15 +16,18 @@ openssl rand -hex 10 > keystone_admin_token
 
 # Install keystone from source
 git clone https://git.openstack.org/openstack/keystone.git
+pacman -S apache --noconfirm
 cd keystone;
-pip install .
-useradd -r -s /usr/bin/nologin keystone
+pip install . pymysql
+useradd -r -s /usr/bin/nologin keystone -m
 
 # Populate /etc
 mkdir /etc/keystone
 cd etc
 cp keystone.conf.sample /etc/keystone/keystone.conf
-cp default_catalog.templates keystone-paste.ini policy.json sso_callback_template.html
+cp logging.conf.sample /etc/keystone/logging.conf
+cp default_catalog.templates keystone-paste.ini policy.json sso_callback_template.html /etc/keystone
+cd $root_path
 
 # Configure keystone
 sed -i "/^\[DEFAULT\]/a admin_token = $(cat keystone_admin_token)" /etc/keystone/keystone.conf
@@ -36,7 +40,7 @@ keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
 # Install apache
 pacman -S apache mod_wsgi --noconfirm
 sed -i "s/#ServerName.*/ServerName $HOSTNAME/" /etc/httpd/conf/httpd.conf
-cp keystone/httpd/wsgi-keystone.conf /etc/httpd/conf/extra/wsgi-keystone.conf
+cp wsgi-keystone.conf /etc/httpd/conf/extra/wsgi-keystone.conf
 sed -i "/^#Include conf\/extra\/httpd-ssl.conf.*/a Include conf\/extra\/wsgi-keystone.conf" /etc/httpd/conf/httpd.conf
 sed -i "/^#LoadModule rewrite_module.*/a LoadModule wsgi_module modules/mod_wsgi.so" /etc/httpd/conf/httpd.conf
 sed -i 's/apache2/httpd/g' /etc/httpd/conf/extra/wsgi-keystone.conf
