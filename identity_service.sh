d0da7df6466f73dde20466e843e04d961ccae69a
root <root@archqed.default>
2016-08-20 02:10:59 -0500

    Fixing wsgi module to python2

--- identity_service.sh
+++ identity_service.sh
@@ -38,7 +38,7 @@ su -s /bin/sh -c "keystone-manage db_sync" keystone
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
 
 # Install apache
-pacman -S apache mod_wsgi --noconfirm
+pacman -S apache mod_wsgi2 --noconfirm
 sed -i "s/#ServerName.*/ServerName $HOSTNAME/" /etc/httpd/conf/httpd.conf
 cp wsgi-keystone.conf /etc/httpd/conf/extra/wsgi-keystone.conf
 sed -i "/^#Include conf\/extra\/httpd-ssl.conf.*/a Include conf\/extra\/wsgi-keystone.conf" /etc/httpd/conf/httpd.conf
